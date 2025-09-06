f7df73b010eb6b26b81f8965f68669e408fb1a91
Sachico <s104103045@gmail.com>
2025-09-06 10:37:30 +0800

    filter status css

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -17,9 +17,30 @@ const filterStatus = ref('all')
 <template>
   <div class="todoList_list">
     <ul class="todoList_tab">
-      <li><a href="#" @click.prevent="filterStatus = 'all'" class="active">全部</a></li>
-      <li><a href="#" @click.prevent="filterStatus = 'incomplete'">待完成</a></li>
-      <li><a href="#" @click.prevent="filterStatus = 'completed'">已完成</a></li>
+      <li>
+        <a
+          href="#"
+          @click.prevent="filterStatus = 'all'"
+          :class="{ active: filterStatus === 'all' }"
+          >全部</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          @click.prevent="filterStatus = 'incomplete'"
+          :class="{ active: filterStatus === 'incomplete' }"
+          >待完成</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          @click.prevent="filterStatus = 'completed'"
+          :class="{ active: filterStatus === 'completed' }"
+          >已完成</a
+        >
+      </li>
     </ul>
     {{ filterStatus }}
     <div class="todoList_items">
